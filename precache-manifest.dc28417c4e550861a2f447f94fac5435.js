63b7eccdec3f3e378f8df4f3d1f3a7ce65ffcf5b
le phung <lephung@les-MacBook-Pro-2.local>
2020-07-04 19:01:24 +0700

    Updates

---
+++ precache-manifest.dc28417c4e550861a2f447f94fac5435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2aa4072984d6f72c410979b05c21dba",
+    "url": "/NC-mini-shop/index.html"
+  },
+  {
+    "revision": "158bd0cfa58cc2ee2c08",
+    "url": "/NC-mini-shop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9b0813e09a3de6b3fb8c",
+    "url": "/NC-mini-shop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "158bd0cfa58cc2ee2c08",
+    "url": "/NC-mini-shop/static/js/2.95b96085.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/NC-mini-shop/static/js/2.95b96085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0813e09a3de6b3fb8c",
+    "url": "/NC-mini-shop/static/js/main.4d8b7edf.chunk.js"
+  },
+  {
+    "revision": "0925769eb7ad95a7c752",
+    "url": "/NC-mini-shop/static/js/runtime-main.2701fda8.js"
+  }
+]);
